d1a48119d48e39b7b30da709a1b587cecbb0f833
pathum herath <pathum@ideahub.lk>
2019-09-26 21:27:00 +0530

    add basic folder and component

--- src/App.js
+++ src/App.js
@@ -2,101 +2,11 @@ import React from 'react';
 import { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Person from './Person/Person';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component{
-  state = {
-    persons: [
-      {
-        name: "max",
-        age: "24"
-      },
-      {
-        name: "manu",
-        age: "27"
-      },
-      {
-        name: "salu",
-        age: "35"
-      }
-    ]
-  };
 
-  switchNameHandler = (newName) => {
-
-    this.setState(
-      {
-        persons: [
-          {
-            name: newName,
-            age: "24"
-          },
-          {
-            name: "manu",
-            age: "27"
-          },
-          {
-            name: "pathum herath",
-            age: "35"
-          }
-        ]
-      }
-    )
-  };
-
-  nameChangedHandler = (event) => {
-    this.setState(
-      {
-        persons: [
-          {
-            name: event.target.value,
-            age: "24"
-          },
-          {
-            name: event.target.value,
-            age: "27"
-          },
-          {
-            name: "pathum herath",
-            age: "35"
-          }
-        ]
-      }
-    )
-  }
-
-  render(){
-    const style = {
-      backgroundColor: 'gray',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    }
-    return (
-      <div className="App">
-      <h1>Hello world</h1>
-      <p>first react app</p>
-      <button style = {style} onClick = {( ) => this.switchNameHandler("Maxmillion") }>Switch Person</button>
-      <Person
-      name = { this.state.persons[0].name }
-      age = { this.state.persons[0].age }
-      change = { this.nameChangedHandler }
-      />
-      <Person
-      name = { this.state.persons[1].name }
-      age = { this.state.persons[1].age }
-      click = { this.switchNameHandler.bind(this, "max!") }
-      change = { this.nameChangedHandler }>
-      Children Property
-      </Person>
-      <Person
-      name = { this.state.persons[2].name }
-      age = { this.state.persons[2].age }
-      />
-    </div>
-    )
-  }
 }
 
 export default App;
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserOutput = (props) => {
+
+}
+
+export default UserOutput;
