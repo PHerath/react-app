afa6191fe38e934873bdb78371d23f34a8c57a66
pathum herath <pathum@ideahub.lk>
2019-09-26 21:16:01 +0530

    add inner style using css to App component

--- src/App.js
+++ src/App.js
@@ -66,11 +66,18 @@ class App extends Component{
   }
 
   render(){
+    const style = {
+      backgroundColor: 'gray',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return (
       <div className="App">
       <h1>Hello world</h1>
       <p>first react app</p>
-      <button onClick = {( ) => this.switchNameHandler("Maxmillion") }>Switch Person</button>
+      <button style = {style} onClick = {( ) => this.switchNameHandler("Maxmillion") }>Switch Person</button>
       <Person
       name = { this.state.persons[0].name }
       age = { this.state.persons[0].age }
