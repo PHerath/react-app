9578f631f1164c53596ba3ed21d3cf0d2c53dbd6
pathum herath <pathum@ideahub.lk>
2019-09-27 11:55:26 +0530

    delete persons and update persons state

--- src/App.js
+++ src/App.js
@@ -45,6 +45,12 @@ class App extends Component{
     )
   };
 
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
+  }
+
   togglePersonHandler = () => {
       const doesShow = this.state.showPersons;
       this.setState({showPersons: !doesShow})
@@ -86,8 +92,9 @@ class App extends Component{
         persons = (
             <div>
                 {
-                    this.state.persons.map( person => {
+                    this.state.persons.map( (person, index) => {
                         return <Person
+                        click = {() => this.deletePersonHandler(index)}
                         name = {person.name}
                         age = {person.age}/>
                     })
