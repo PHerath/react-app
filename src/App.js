a54c997a25f4e3854167561045a036b54979a2ac
pathum herath <pathum@ideahub.lk>
2019-09-30 13:45:45 +0530

    create Validate, Char components

--- src/App.js
+++ src/App.js
@@ -2,115 +2,105 @@ import React from 'react';
 import { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Person from './Person/Person';
+import Validate from './Validate/Validate';
+import Char from './Char/Char';
 
 class App extends Component{
   state = {
-    persons: [
-      {
-          id: "adaf1",
-          name: "max",
-          age: "24"
-      },
-      {
-          id: "iuybfuwu2",
-          name: "manu",
-          age: "27"
-      },
-      {
-          id: "qnbvu3",
-          name: "salu",
-          age: "35"
-      }
-    ],
-    showPersons: false
+    letters: [],
+    length: 0,
+    showLetters: false
   };
 
-//   switchNameHandler = (newName) => {
+//   deletePersonHandler = (personIndex) => {
+//     //const persons = this.state.persons; this is danger use as does not copy object and reference to direct object
+//     //const persons = this.state.persons.slice(); this is old way copy an object
+//     const persons = [...this.state.persons]; // this is modern way copy an object
+//     persons.splice(personIndex, 1);
+//     this.setState({persons: persons})
+//   }
 
-//     this.setState(
-//       {
-//         persons: [
-//           {
-//             name: newName,
-//             age: "24"
-//           },
-//           {
-//             name: "manu",
-//             age: "27"
-//           },
-//           {
-//             name: "pathum herath",
-//             age: "35"
-//           }
-//         ]
-//       }
-//     )
+//   togglePersonHandler = () => {
+//       const doesShow = this.state.showPersons;
+//       this.setState({showPersons: !doesShow})
 //   };
 
-  deletePersonHandler = (personIndex) => {
-    //const persons = this.state.persons; this is danger use as does not copy object and reference to direct object
-    //const persons = this.state.persons.slice(); this is old way copy an object
-    const persons = [...this.state.persons]; // this is modern way copy an object
-    persons.splice(personIndex, 1);
-    this.setState({persons: persons})
+//   nameChangedHandler = (event, id) => {
+//       const personIndex = this.state.persons.findIndex(per => {
+//           return per.id === id;
+//       });
+//       //let person = Object.assign({}, this.state.persons[personIndex]); this is old way
+
+//       //modern way
+//       let person = {
+//           ...this.state.persons[personIndex]
+//       }
+//       person.name = event.target.value;
+//       const persons = this.state.persons;
+//       persons[personIndex] = person;
+//       this.setState({persons: persons});
+//   };
+  handleTextLength = (event) => {
+    const val = event.target.value
+    const charArray = val.replace(" ", "").split("")
+
+    this.setState({
+      letters: charArray,
+      length: charArray.length
+    })
   }
-
-  togglePersonHandler = () => {
-      const doesShow = this.state.showPersons;
-      this.setState({showPersons: !doesShow})
-  };
-
-  nameChangedHandler = (event, id) => {
-      const personIndex = this.state.persons.findIndex(per => {
-          return per.id === id;
-      });
-      //let person = Object.assign({}, this.state.persons[personIndex]); this is old way
-
-      //modern way
-      let person = {
-          ...this.state.persons[personIndex]
-      }
-      person.name = event.target.value;
-      const persons = this.state.persons;
-      persons[personIndex] = person;
-      this.setState({persons: persons});
-  };
-
   render(){
-    const style = {
-      backgroundColor: 'gray',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
+
+    let characters = null;
+    // const style = {
+    //   backgroundColor: 'gray',
+    //   font: 'inherit',
+    //   border: '1px solid blue',
+    //   padding: '8px',
+    //   cursor: 'pointer'
+    // }
+
+    // let persons = null;
+
+    // if(this.state.showPersons){
+    //     persons = (
+    //         <div>
+    //             {
+    //                 this.state.persons.map( (person, index) => {
+    //                     return <Person
+    //                     click = {() => this.deletePersonHandler(index)}
+    //                     name = {person.name}
+    //                     age = {person.age}
+    //                     key = {person.id}
+    //                     changed = {(event) => this.nameChangedHandler(event, person.id)}
+    //                     />
+    //                 })
+    //             }
+    //     </div>
+    //     )
+    // };
+
+    if(this.state.showLetters){
+      characters = (
+        <div>
+          {
+            this.state.charArray.map(chr => {
+              return (
+                <Char char = {chr}></Char>
+              )
+            })
+          }
+        </div>
+      )
     }
 
-    let persons = null;
-
-    if(this.state.showPersons){
-        persons = (
-            <div>
-                {
-                    this.state.persons.map( (person, index) => {
-                        return <Person
-                        click = {() => this.deletePersonHandler(index)}
-                        name = {person.name}
-                        age = {person.age}
-                        key = {person.id}
-                        changed = {(event) => this.nameChangedHandler(event, person.id)}
-                        />
-                    })
-                }
-        </div>
-        )
-    };
     return (
       <div className="App">
       <h1>Hello world</h1>
       <p>first react app</p>
-      <button style = {style} onClick = {this.togglePersonHandler}>Switch Person</button>
-      {persons}
+      <input type = "text" onChange = {this.handleTextLength}></input>
+      <Validate length = {this.state.length}></Validate>
+      {characters}
     </div>
     )
   }
---
+++ src/Validate/Validate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Validate = (props) => {
+    return (
+        <div>
+            <p>text length is {props.length}</p>
+        </div>
+    )
+}
+
+export default Validate;
